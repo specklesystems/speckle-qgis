b73ae8155a51932e947f0a0a9063966b6fabc774
KatKatKateryna <kateryna@speckle.systems>
2022-11-15 14:58:12 +0800

    temporary solution to large polygons: reducing to 5000 points without holes; or to 150 if with holes

--- speckle/converter/geometry/polygon.py
+++ speckle/converter/geometry/polygon.py
@@ -83,68 +83,86 @@ def polygonToSpeckle(geom: QgsGeometry, feature: QgsFeature, layer: QgsVectorLay
             except: pass # if Line
         #polyBorder = boundary.as_points()
 
+        coef = 1
+        maxPoints = 5000
+        if len(polyBorder) >= maxPoints: coef = int(len(polyBorder)/maxPoints)
+            
         if len(voids) == 0: # if there is a mesh with no voids
-            for pt in polyBorder: #pointList:
-                if isinstance(pt, QgsPointXY):
-                    pt = QgsPoint(pt)
-                if isinstance(pt,Point):
-                    pt = pointToNative(pt)
-                x = pt.x()
-                y = pt.y()
-                z = 0 if math.isnan(pt.z()) else pt.z()
-                vertices.extend([x, y, z])
-                total_vertices += 1
+            for k, ptt in enumerate(polyBorder): #pointList:
+                pt = polyBorder[k*coef]
+                if k < maxPoints:
+                    if isinstance(pt, QgsPointXY):
+                        pt = QgsPoint(pt)
+                    if isinstance(pt,Point):
+                        pt = pointToNative(pt)
+                    x = pt.x()
+                    y = pt.y()
+                    z = 0 if math.isnan(pt.z()) else pt.z()
+                    vertices.extend([x, y, z])
+                    total_vertices += 1
+                else: break
 
             ran = range(0, total_vertices)
             faces = [total_vertices]
             faces.extend([i for i in ran])
             # else: https://docs.panda3d.org/1.10/python/reference/panda3d.core.Triangulator
         else: # if there are voids 
+            # if its a large polygon with voids to be triangualted, lower the coef even more:
+            maxPoints = 150
+            if len(polyBorder) >= maxPoints: coef = int(len(polyBorder)/maxPoints)
+
             trianglator = Triangulator()
             faces = []
 
-
             pt_count = 0
             # add extra middle point for border
-            for pt in polyBorder:
-              if pt_count < len(polyBorder)-1: 
-                  pt2 = polyBorder[pt_count+1]
-              else: pt2 = polyBorder[0]
-              
-              trianglator.addPolygonVertex(trianglator.addVertex(pt.x, pt.y))
-              vertices.extend([pt.x, pt.y, pt.z])
-              trianglator.addPolygonVertex(trianglator.addVertex((pt.x+pt2.x)/2, (pt.y+pt2.y)/2))
-              vertices.extend([(pt.x+pt2.x)/2, (pt.y+pt2.y)/2, (pt.z+pt2.z)/2])
-              total_vertices += 2
-              pt_count += 1
+            for k, ptt in enumerate(polyBorder): #pointList:
+                pt = polyBorder[k*coef]
+                if k < maxPoints:
+                    if pt_count < len(polyBorder)-1 and k < (maxPoints-1): 
+                        pt2 = polyBorder[(k+1)*coef]
+                    else: pt2 = polyBorder[0]
+                            
+                    trianglator.addPolygonVertex(trianglator.addVertex(pt.x, pt.y))
+                    vertices.extend([pt.x, pt.y, pt.z])
+                    trianglator.addPolygonVertex(trianglator.addVertex((pt.x+pt2.x)/2, (pt.y+pt2.y)/2))
+                    vertices.extend([(pt.x+pt2.x)/2, (pt.y+pt2.y)/2, (pt.z+pt2.z)/2])
+                    total_vertices += 2
+                    pt_count += 1
+                else: break
 
             #add void points
             for i in range(len(voids)):
-              trianglator.beginHole()
+                trianglator.beginHole()
 
-              pts = []
-              if isinstance(voids[i], Circle) or isinstance(voids[i], Arc): 
-                  pts = speckleArcCircleToPoints(voids[i]) 
-              elif isinstance(voids[i], Polycurve): 
-                  pts = specklePolycurveToPoints(voids[i]) 
-              elif isinstance(voids[i], Line): pass
-              else: 
-                  try: pts = voids[i].as_points()
-                  except: pass # if Line
+                pts = []
+                if isinstance(voids[i], Circle) or isinstance(voids[i], Arc): 
+                    pts = speckleArcCircleToPoints(voids[i]) 
+                elif isinstance(voids[i], Polycurve): 
+                    pts = specklePolycurveToPoints(voids[i]) 
+                elif isinstance(voids[i], Line): pass
+                else: 
+                    try: pts = voids[i].as_points()
+                    except: pass # if Line
 
-              #pts = voids[i].as_points()
-              for pt in pts:
-                trianglator.addHoleVertex(trianglator.addVertex(pt.x, pt.y))
-                vertices.extend([pt.x, pt.y, pt.z])
-                total_vertices += 1
-
+                #pts = voids[i].as_points()
+                coefVoid = 1
+                if len(pts) >= maxPoints: coefVoid = int(len(pts)/maxPoints)
+                for k, ptt in enumerate(pts):
+                    pt = pts[k*coefVoid]
+                    if k < maxPoints:
+                        trianglator.addHoleVertex(trianglator.addVertex(pt.x, pt.y))
+                        vertices.extend([pt.x, pt.y, pt.z])
+                        total_vertices += 1
+                    else: break
+            
             trianglator.triangulate()
             i = 0
             #print(trianglator.getNumTriangles())
             while i < trianglator.getNumTriangles():
-              tr = [trianglator.getTriangleV0(i),trianglator.getTriangleV1(i),trianglator.getTriangleV2(i)]
-              faces.extend([3, tr[0], tr[1], tr[2]])
-              i+=1
+                tr = [trianglator.getTriangleV0(i),trianglator.getTriangleV1(i),trianglator.getTriangleV2(i)]
+                faces.extend([3, tr[0], tr[1], tr[2]])
+                i+=1
             ran = range(0, total_vertices)
 
         col = featureColorfromNativeRenderer(feature, layer)
