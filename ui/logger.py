b4612b2971a014bf09856378ad7c8bf4e005456c
KatKatKateryna <kateryna@speckle.systems>
2023-03-16 17:57:59 +0000

    msg split into lines

--- ui/logger.py
+++ ui/logger.py
@@ -13,7 +13,8 @@ def logToUserWithAction(msg: str, func=None, level: int = 2, action_text="Click"
       print("Log to user with action")
       window = createWindow(msg, func, level)
       
-      window.exec_() 
+      if window is not None: 
+            window.exec_() 
       return 
       
       if callback is not None:
@@ -24,25 +25,42 @@ def logToUserWithAction(msg: str, func=None, level: int = 2, action_text="Click"
       window.exec_() 
       return
 
-def createWindow(msg: str, func=None, level: int = 2):
+def createWindow(msg_old: str, func=None, level: int = 2):
       print("Create window")
-      # https://www.techwithtim.net/tutorials/pyqt5-tutorial/messageboxes/
-      window = QMessageBox()
-      window.setWindowFlag(QtCore.Qt.WindowStaysOnTopHint)
-      if level==0: 
-            window.setWindowTitle("Info")
-            window.setIcon(QMessageBox.Icon.Information)
-      if level==1: 
-            window.setWindowTitle("Warning")
-            window.setIcon(QMessageBox.Icon.Warning)
-      elif level==2: 
-            window.setWindowTitle("Error")
-            window.setIcon(QMessageBox.Icon.Critical)
-      window.setFixedWidth(200)
+      window = None
+      try:
+            # https://www.techwithtim.net/tutorials/pyqt5-tutorial/messageboxes/
+            window = QMessageBox()
+            msg = ""
+            if len(msg_old)>60:
+                  try:
+                        for i, x in enumerate(msg_old):
+                              print(x)
+                              msg += x
+                              if i!=0 and i%60 == 0: msg += "\n"
+                              print(msg)
+                  except Exception as e: print(e)
+            else: 
+                  msg = msg_old
 
-      if func is not None:
-            window.setText(str(msg + "\n" + str(func)))
-      else: 
-            window.setText(str(msg))
-      print(window)
+            print(msg)
+      
+            window.setWindowFlag(QtCore.Qt.WindowStaysOnTopHint)
+            if level==0: 
+                  window.setWindowTitle("Info")
+                  window.setIcon(QMessageBox.Icon.Information)
+            if level==1: 
+                  window.setWindowTitle("Warning")
+                  window.setIcon(QMessageBox.Icon.Warning)
+            elif level==2: 
+                  window.setWindowTitle("Error")
+                  window.setIcon(QMessageBox.Icon.Critical)
+            window.setFixedWidth(200)
+
+            if func is not None:
+                  window.setText(str(msg + "\n" + str(func)))
+            else: 
+                  window.setText(str(msg))
+            print(window)
+      except Exception as e: print(e)
       return window 
