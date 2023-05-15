415c40a0c16b7ac7b84e22282e336f87b2d17590
KatKatKateryna <kateryna@speckle.systems>
2023-05-15 01:53:15 +0100

    crs XY origin unused yet

--- speckle/converter/layers/CRS.py
+++ speckle/converter/layers/CRS.py
@@ -6,16 +6,12 @@ class CRS(Base):
     """A very basic GIS Coordinate Reference System stored in wkt format"""
     name: Union[str,None]
     wkt: Union[str,None]
-    x_origin: Optional[float]
-    y_origin: Optional[float]
     units: Union[str,None]
 
     def __init__(
         self, 
         name: Union[str,None] = None, 
         wkt: Union[str,None] = None, 
-        x_origin:Optional[float] = None,
-        y_origin:Optional[float] = None,
         units: Union[str,None] = "m", 
         **kwargs
     ) -> None:
@@ -23,6 +19,4 @@ class CRS(Base):
 
         self.name = name if name != None else ""
         self.wkt = wkt if wkt != None else ""
-        self.x_origin = x_origin 
-        self.y_origin = y_origin 
         self.units = units if units != None else "m"
