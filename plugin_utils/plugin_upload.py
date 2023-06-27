3b9f68f44ab6555d22c2284225aa7bf103dc44ab
KatKatKateryna <kateryna@speckle.systems>
2023-06-27 16:14:26 +0100

    print data

--- plugin_utils/plugin_upload.py
+++ plugin_utils/plugin_upload.py
@@ -38,10 +38,10 @@ def main(parameters, arguments):
     server = xmlrpc.client.ServerProxy(address, verbose=VERBOSE)
 
     try:
+        print(arguments[0])
+        print(server)
+        print(server.plugin)
         with open(arguments[0], 'rb') as handle:
-            print(server)
-            print(server.plugin)
-            print(handle.read())
             plugin_id, version_id = server.plugin.upload(
                 xmlrpc.client.Binary(handle.read()))
         print("Plugin ID: %s" % plugin_id)
