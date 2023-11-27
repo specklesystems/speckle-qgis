7d1764fd2b4a05dd9b73b909d1478255dc967feb
KatKatKateryna <kateryna@speckle.systems>
2023-11-27 21:27:59 +0800

    debugger off

--- plugin_utils/installer.py
+++ plugin_utils/installer.py
@@ -13,7 +13,7 @@ import shutil
 from speckle.utils.utils import get_qgis_python_path
 
 _user_data_env_var = "SPECKLE_USERDATA_PATH"
-_debug = True
+_debug = False
 _vs_code_directory = os.path.expanduser(
     "~\.vscode\extensions\ms-python.python-2023.20.0\pythonFiles\lib\python"
 )
