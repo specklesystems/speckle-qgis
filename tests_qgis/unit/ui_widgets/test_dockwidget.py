6510b5e5ad93a3a5bf9e3255b33f96dd349a7784
KatKatKateryna <kateryna@speckle.systems>
2024-03-14 11:39:21 +0100

    clean tests for now

--- tests_qgis/unit/ui_widgets/test_dockwidget.py
+++
@@ -1,7 +0,0 @@
-from speckle.ui_widgets.dockwidget_main import SpeckleQGISDialog
-
-
-def test_create_widget():
-    # widget = SpeckleQGISDialog()
-    # widget.createMappingDialog()
-    assert True
--- tests_qgis/unit/ui_widgets/test_transform_widget.py
+++
@@ -1,7 +0,0 @@
-from speckle.ui_widgets.widget_transforms import MappingSendDialogQGIS
-
-
-def test_create_widget():
-    # widget = MappingSendDialogQGIS()
-    # widget.runSetup()
-    assert True
