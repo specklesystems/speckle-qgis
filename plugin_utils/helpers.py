de002e07f095db88b284cfc0ca4fddad8fa09966
KatKatKateryna <kateryna@speckle.systems>
2024-06-05 17:33:55 +0100

    remove specklepy call in helpers

--- plugin_utils/helpers.py
+++ plugin_utils/helpers.py
@@ -5,8 +5,6 @@ from textwrap import wrap
 import inspect
 from difflib import SequenceMatcher
 
-from specklepy.objects.units import get_units_from_string
-
 SYMBOL = "_x_x_"
 UNSUPPORTED_PROVIDERS = ["WFS", "wms", "wcs", "vectortile"]
 
@@ -33,8 +31,13 @@ def get_scale_factor(units: str, dataStorage) -> float:
         return scale_to_meter
 
 
-def get_scale_factor_to_meter(units_src: str) -> float:
-    units = str(get_units_from_string(units_src))
+def get_scale_factor_to_meter(units) -> float:
+    if not isinstance(units, str):
+        try:
+            units = units.value
+        except:
+            units = str(units)
+    units = units.lower()
     try:
         unit_scale = {
             "meters": 1.0,
