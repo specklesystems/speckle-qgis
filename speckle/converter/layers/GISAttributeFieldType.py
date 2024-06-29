635d4c2d6315b088e32d5e569b882995e0234cb9
KatKatKateryna <kateryna@speckle.systems>
2024-06-29 23:25:18 +0100

    field types methods

--- speckle/converter/layers/GISAttributeFieldType.py
+++ speckle/converter/layers/GISAttributeFieldType.py
@@ -16,61 +16,42 @@ class GISAttributeFieldType(str, Enum):
     DATEONLY = "DateOnly"
     TIMEONLY = "TimeOnly"
     TIMESTAMPOFFSET = "TimeStampOffset"
+    BOOL = "Bool"
 
-    r"""
-    qgis_types = [
-        (1, "bool"), 
-        (2, "int"),
-        (6, "decimal"),
-        (8, "map"),
-        (9, "int_list"),
-        (10, "string"),
-        (11, "string_list"),
-        (12, "binary"),
-        (14, "date"),
-        (15, "time"),
-        (16, "date_time") 
-    ]
     @staticmethod
-    def assign_speckle_layer_geometry_type(native_geom_type: int):
+    def assign_speckle_field_type(native_geom_type: int):
 
         type_index: int = native_geom_type % 1000
         val_dict = {
-            1: GISLayerGeometryType.POINT,
-            2: GISLayerGeometryType.POLYLINE,
-            3: GISLayerGeometryType.POLYGON,
-            4: GISLayerGeometryType.POINT,
-            5: GISLayerGeometryType.POLYLINE,
-            6: GISLayerGeometryType.POLYGON,
-            7: "GeometryCollection",
-            8: GISLayerGeometryType.POLYLINE,
-            9: GISLayerGeometryType.POLYLINE,
-            10: GISLayerGeometryType.POLYGON,
-            11: GISLayerGeometryType.POLYLINE,
-            12: "MultiSurface",
-            17: "Triangle",
+            1: GISAttributeFieldType.BOOL,
+            2: GISAttributeFieldType.INTEGER_TYPE,
+            6: GISAttributeFieldType.DOUBLE_TYPE,
+            10: GISAttributeFieldType.STRING_TYPE,
+            14: GISAttributeFieldType.DATEONLY,
+            15: GISAttributeFieldType.TIMEONLY,
+            16: GISAttributeFieldType.DATETIME,
         }
-        result: str = val_dict.get(type_index, GISLayerGeometryType.NONE)
-
-        # write 3d polygons as a separate type
-        if native_geom_type > 1000 and result == GISLayerGeometryType.POLYGON:
-            result = GISLayerGeometryType.POLYGON3D
-
+        result: str = val_dict.get(type_index, GISAttributeFieldType.STRING_TYPE)
         return result
 
     @staticmethod
-    def get_native_layer_geometry_type_from_speckle(string_geom_type: str):
+    def get_native_field_type_from_speckle(string_geom_type: str):
 
-        speckle_type = GISLayerGeometryType(string_geom_type)
+        speckle_type = GISAttributeFieldType(string_geom_type)
         val_dict = {
-            GISLayerGeometryType.NONE: "None",
-            GISLayerGeometryType.POINT: "MultiPointZ",
-            GISLayerGeometryType.POLYLINE: "MultiLineStringZ",
-            GISLayerGeometryType.POLYGON: "MultiPolygon",
-            GISLayerGeometryType.POLYGON3D: "MultiPolygonZ",
-            GISLayerGeometryType.MULTIPATCH: "MultiPolygonZ",
-            # GISLayerGeometryType.POINTCLOUD: ,
+            GISAttributeFieldType.GUID_TYPE: "Guid",
+            GISAttributeFieldType.OID: "Oid",
+            GISAttributeFieldType.STRING_TYPE: "String",
+            GISAttributeFieldType.FLOAT_TYPE: "Float",
+            GISAttributeFieldType.INTEGER_TYPE: "Integer",
+            GISAttributeFieldType.BIGINTEGER: "BigInteger",
+            GISAttributeFieldType.SMALLINTEGER: "SmallInteger",
+            GISAttributeFieldType.DOUBLE_TYPE: "Double",
+            GISAttributeFieldType.DATETIME: "DateTime",
+            GISAttributeFieldType.DATEONLY: "DateOnly",
+            GISAttributeFieldType.TIMEONLY: "TimeOnly",
+            GISAttributeFieldType.TIMESTAMPOFFSET: "TimeStampOffset",
+            GISAttributeFieldType.BOOL: "Bool",
         }
-        result: str = val_dict.get(speckle_type, "None")
+        result: str = val_dict.get(speckle_type, GISAttributeFieldType.STRING_TYPE)
         return result
-    """
